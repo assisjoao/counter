c7a1c93915af6b8edb5b6029af49002b8bbd6847
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-20 07:56:05 -0300

    start counter

---
+++ src/componentes/Contador.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Contador(props){
+
+    return (<div>
+        <div>0</div>
+        <button>-</button>
+        <button>+</button>
+
+    </div>)
+}
+
+export default Contador;
